5544c5c083e670f18b225f91270551e3aa19a635
Stefan <stefanroetscode@gmail.com>
2020-05-13 08:33:08 +0200

    Variables

---
+++ Stefan try one/src/storry.java
@@ -0,0 +1,19 @@
+
+public class storry {
+	public static void main(String []args) {
+		
+		String characterName = "George";
+		String charactSurName = " Lopez";
+		int characterAge = 20;
+		
+		System.out.println("There once was a man named "  + characterName + charactSurName );
+		System.out.println("He was " + characterAge + " years old. He really liked");
+		
+		characterName = "Johnny";
+		characterAge = 45;
+		
+		System.out.println("the name " + characterName + ", But didn't like being 40.");
+		System.out.println("Most of his friends were also " + characterAge + ", and one");
+		System.out.println("of them was also name " + characterName + ".");
+		}
+}
