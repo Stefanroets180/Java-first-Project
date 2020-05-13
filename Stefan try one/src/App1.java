083056cf5dc9b3246c913914844a424a41a52a76
Stefan <stefanroetscode@gmail.com>
2020-05-13 14:13:03 +0200

    Chapter 6 Data Types

---
+++ Stefan try one/src/App1.java
@@ -0,0 +1,14 @@
+
+public class App1 {
+	public static void main(String[] args) {
+		
+		String phrase = "To be or not to be";
+		char grade = 'A';            // string one single carter 1
+		int age = 35;               //hall number integer 
+		double gpa = 2.7;          // Floating point numbers or dismal numbers
+		boolean isMale = true;    //true or false value
+		
+		System.out.println(isMale);
+	}
+}
+  
